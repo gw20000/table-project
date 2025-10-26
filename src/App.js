dabda75ef8a4626a609757f30dace9cf715eac7d
Rainey <changjiu1391@126.com>
2025-10-26 16:32:40 +0800

    test auto deploy new commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import TableComponent from './components/TableComponent.tsx';
 // import Counter from './components/Counter.tsx';
 // import TodoList from './components/TodoList.tsx';
-// import CountDown from './components/CountDown.tsx';
+import CountDown from './components/CountDown.tsx';
 
 
 function App() {
   return <div className="app">
+    <CountDown endTime={new Date('2025-11-25')} />
     <TableComponent />
     {/* <Counter />
     <TodoList /> */}
-    {/* <CountDown endTime={new Date('2025-11-25')} /> */}
+
   </div>
 
 }
